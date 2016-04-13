6e888510d1fd8b2f7ee1e53aa697e59066881a1f
Ruben ROdriguez <rubenrodriguezfdz@gmail.com>
2016-04-13 12:08:06 +0200

    Incluide database to the project and manage variables of the templates
       TASK:
    	- Create migrations
    	- Create seedings
    	- Implement set variables of the template

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/getAllTemplates', 'TemplateController@getAllTemplates');
+
+Route::get('/getCreatedTemplates', 'TemplateController@getCreatedTemplates');
+
+Route::get('/getTemplate/{id}', 'TemplateController@getTemplate');
